6602cdb6fad298350c24214ab10a74c39009f62e
burakvarol-tr <burakvarol0626@gmail.com>
2026-07-08 13:45:35 +0300

    Update mailRecipients.ts

--- data/mailRecipients.ts
+++ data/mailRecipients.ts
@@ -1,3 +1,7 @@
 export const PRICE_ALERT_RECIPIENTS = [
   "burak.varol@goknur.com.tr",
+  "selin.kilic@goknur.com.tr",
+  "aliaslanali@goknur.com.tr",
+  "nazlican.aydogan@goknur.com.tr",
+  "melih.duzgun@goknur.com.tr",
 ];

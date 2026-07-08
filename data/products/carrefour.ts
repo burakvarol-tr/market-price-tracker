82c391dbd214e64eed8be5e5635bfb919994d3e2
burakvarol-tr <burakvarol0626@gmail.com>
2026-07-08 13:50:12 +0300

    Update carrefour.ts

--- data/products/carrefour.ts
+++ data/products/carrefour.ts
@@ -1,3 +1,8 @@
 import type { TrackedProduct } from "@/lib/getPrice";
-
-export const CARREFOUR_PRODUCTS: TrackedProduct[] = [];
+export const CARREFOUR_PRODUCTS: TrackedProduct[] = [
+{
+    sku: "30510027",
+    name: "Bonheur %100 Elma Vişne 1 L",
+    market: "CARREFOUR",
+  },
+];

37d6ce883eccfacc54b1f2a7061d1d13664cebbd
burakvarol-tr <burakvarol0626@gmail.com>
2026-08-03 16:30:39 +0300

    Update carrefour.ts

--- data/products/carrefour.ts
+++ data/products/carrefour.ts
@@ -1,3 +1,34 @@
 import type { TrackedProduct } from "@/lib/getPrice";
 
-export const CARREFOUR_PRODUCTS: TrackedProduct[] = [];
+export const CARREFOUR_PRODUCTS: TrackedProduct[] = [
+  {
+    sku: "30509958",
+    name: "Bonheur %100 Elma Suyu 1 L",
+    market: "CARREFOUR",
+  },
+  {
+    sku: "30510027",
+    name: "Bonheur %100 Elma Vişne 1 L",
+    market: "CARREFOUR",
+  },
+  {
+    sku: "30510076",
+    name: "Bonheur %100 Şeftali Elma Suyu 1 L",
+    market: "CARREFOUR",
+  },
+  {
+    sku: "30510077",
+    name: "Bonheur %100 Karışık Meyve Suyu 1 L",
+    market: "CARREFOUR",
+  },
+  {
+    sku: "30511967",
+    name: "Bonheur Limonata 1 L",
+    market: "CARREFOUR",
+  },
+  {
+    sku: "30512090",
+    name: "Bonheur Limonata Şekersiz Nane Aromalı 1 L",
+    market: "CARREFOUR",
+  },
+];
